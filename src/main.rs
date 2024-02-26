10b8472cd5edb43c03120bcbe57070f26dc88f2e
ZoopGoop <93421308+ZoopOTheGoop@users.noreply.github.com>
2024-02-25 21:31:23 -0800

    wip: vent autodelete

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
+use std::fmt::Write;
+
 use anyhow::anyhow;
-use serenity::Error as SerenityError;
+use chrono::{DateTime, Duration, Utc};
+use poise::{serenity_prelude::*, CreateReply};
+use serenity::{
+    futures::{future, StreamExt, TryStreamExt},
+    Error as SerenityError,
+};
 use shuttle_secrets::SecretStore;
 use thiserror::Error;
+use tokio::time::Instant;
 use tracing::error;
 
-use poise::{serenity_prelude::*, CreateReply};
+use diesel_async::{pooled_connection::deadpool::Pool, AsyncPgConnection, Error};
+
+const METER_LIMIT: usize = 500;
 
 #[derive(Clone)]
 struct Data {
-    _pool: sqlx::PgPool,
+    pool: Pool<AsyncPgConnection>,
 }
 
 #[derive(Error, Debug)]
 enum SlimeError {
-    #[error("an occur occurred within Serenity: {0}")]
+    #[error("an error occurred within Serenity: {0}")]
     SerenityError(#[from] SerenityError),
+    #[error("an error occurred within sqlx: {0}")]
+    DatabaseError(#[from] SqlxError),
 }
 type Context<'a> = poise::Context<'a, Data, SlimeError>;
 
@@ -31,9 +43,146 @@ fn make_uuid_buttons(yes_uuid: &str, no_uuid: &str, disabled: bool) -> CreateAct
     ])
 }
 
-#[poise::command(slash_command)]
-async fn purge_old(ctx: Context<'_>) -> Result<(), SlimeError> {
-    let _channel = ctx.guild_channel().await.unwrap();
+async fn messages_before(
+    ctx: Context<'_>,
+    before: DateTime<Utc>,
+    channel: ChannelId,
+) -> Result<Vec<Message>, SlimeError> {
+    Ok(channel
+        .messages_iter(ctx)
+        .skip_while(|v| {
+            future::ready(
+                v.as_ref()
+                    .map(|msg| msg.timestamp.to_utc() >= before)
+                    .unwrap_or(false),
+            )
+        })
+        .try_collect()
+        .await?)
+}
+
+async fn bulk_delete(
+    ctx: Context<'_>,
+    messages: &[Message],
+    dry_run: bool,
+) -> Result<(), SlimeError> {
+    debug_assert!(
+        messages[messages.len() - 1].timestamp.to_utc() > Utc::now() - Duration::weeks(2)
+    );
+
+    let mut start_time = Instant::now();
+
+    let mut count = 0;
+    for chunk in messages.chunks(100) {
+        if !dry_run {
+            ctx.channel_id().delete_messages(ctx, chunk).await?;
+        }
+        count += 1;
+
+        if count >= METER_LIMIT {
+            tokio::time::sleep_until(start_time + tokio::time::Duration::from_secs(60)).await;
+            count = 0;
+            start_time = Instant::now();
+        }
+    }
+
+    Ok(())
+}
+
+async fn slow_bulk_delete(
+    ctx: Context<'_>,
+    messages: &[Message],
+    dry_run: bool,
+) -> Result<(), SlimeError> {
+    let mut count = 0;
+    let mut start_time = Instant::now();
+
+    for message in messages {
+        if !dry_run {
+            ctx.channel_id().delete_message(ctx, message).await?;
+        }
+        count += 1;
+
+        if count >= METER_LIMIT {
+            tokio::time::sleep_until(start_time + tokio::time::Duration::from_secs(60)).await;
+            count = 0;
+            start_time = Instant::now();
+        }
+    }
+
+    Ok(())
+}
+
+/// Bulk deletes messages from the supplied channel. Warning: This can take a very long time.
+#[poise::command(
+    slash_command,
+    category = "delete",
+    guild_only = true,
+    default_member_permissions = "ADMINISTRATOR"
+)]
+async fn purge_old(
+    ctx: Context<'_>,
+    #[description = "the channel to purge from"] channel: Channel,
+    #[description = "whether to actually run the command or merely show progress as if it were running"]
+    dry_run: Option<bool>,
+) -> Result<(), SlimeError> {
+    let before = Utc::now() - chrono::Duration::days(7);
+    let dry_run = dry_run.unwrap_or(false) || cfg!(debug);
+
+    let conn = ctx.data().pool.acquire().await?;
+
+    ctx.defer().await?;
+
+    let messages = messages_before(ctx, before, channel.id()).await?;
+
+    let bulk_cutoff = Utc::now() - (chrono::Duration::days(13) + chrono::Duration::hours(12));
+
+    let mut content = String::from("I'll help you purge old messages!\n\n");
+    if messages.is_empty() {
+        return Ok(());
+    }
+
+    let (slow_index, mut minutes) = if let Some((idx, msg)) = messages
+        .iter()
+        .enumerate()
+        .find(|(_, msg)| msg.timestamp.to_utc() < bulk_cutoff)
+    {
+        let old_message_count = messages.len() - idx;
+        let minutes_to_delete = (old_message_count as f64) / (METER_LIMIT as f64);
+
+        write!(
+            &mut content,
+            "This deletion has {old_message_count} messages beyond the bulk cutoff window!\n\
+            At a rate of {METER_LIMIT} messages per minute, deleting these will take approximately {minutes_to_delete:.2} minutes.\n\
+            The first message in this set is <{}>, and the last is <{}>.\n\n",
+            messages[messages.len()-1].link(),
+            msg.link(),
+        )
+        .unwrap();
+
+        (Some(idx), minutes_to_delete)
+    } else {
+        (None, 0.)
+    };
+
+    let bulk_count = slow_index.unwrap_or(0);
+    minutes += if bulk_count > 0 {
+        let msgs_per_min = METER_LIMIT * 100;
+        let minutes_to_delete = (bulk_count as f64) / (msgs_per_min as f64);
+        write!(
+            &mut content,
+            "This deletion has {bulk_count} messages that can be *bulk* deleted!\n\
+            At a rate of {msgs_per_min} messages per minute, deleting these will take approximately {minutes_to_delete:.2} minutes.\n\
+            The first message in this set is <{}>, and the last is <{}>.\n\n",
+            messages[bulk_count-1].link(), messages[0].link(),
+        ).unwrap();
+
+        minutes_to_delete
+    } else {
+        0.
+    };
+
+    write!(&mut content, "Overall, this will take {minutes:.2} minutes to complete, starting with the bulk messages. Continue?").unwrap();
 
     let id = ctx.id();
     let yes_uuid: String = format!("{id}-yes");
@@ -42,10 +191,8 @@ async fn purge_old(ctx: Context<'_>) -> Result<(), SlimeError> {
     let buttons = make_uuid_buttons(&yes_uuid, &no_uuid, false);
 
     let reply = CreateReply::default()
-        .content("The first message to be deleted is <foo>, the last is <bar> continue?")
-        .components(vec![buttons])
-        .ephemeral(true);
-
+        .content(content)
+        .components(vec![buttons]);
     ctx.send(reply).await?;
 
     if let Some(interactions) = ComponentInteractionCollector::new(ctx.serenity_context())
@@ -81,10 +228,36 @@ async fn purge_old(ctx: Context<'_>) -> Result<(), SlimeError> {
     Ok(())
 }
 
+/// Sets the channel where bot spam (e.g. status updates) should happen. Default: current channel
+#[poise::command(
+    slash_command,
+    category = "delete",
+    guild_only = true,
+    default_member_permissions = "ADMINISTRATOR"
+)]
+async fn admin_bot_spam_channel(
+    ctx: Context<'_>,
+    #[description = "the channel to purge from"] channel: Option<Channel>,
+) -> Result<(), SlimeError> {
+    let channel = channel.map(|v| v.id()).unwrap_or(ctx.channel_id());
+
+    let mut conn = ctx.data().pool.acquire().await?;
+
+    sqlx::query!(
+        "INSERT INTO bot_spam_channels (guild_id, channel_id) VALUES ({}, {});",
+        ctx.guild_id().unwrap().to_string(),
+        channel.to_string()
+    )
+    .execute(&mut *conn)
+    .await?;
+
+    todo!()
+}
+
 #[shuttle_runtime::main]
 async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
-    #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    #[shuttle_diesel_async::Postgres] pool: Pool<AsyncPgConnection>,
 ) -> shuttle_serenity::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml`
     let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
@@ -101,13 +274,13 @@ async fn serenity(
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![purge_old()],
+            commands: vec![purge_old(), admin_bot_spam_channel()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data { _pool: pool })
+                Ok(Data { pool })
             })
         })
         .build();
